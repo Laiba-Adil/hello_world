02b5e0c4d8d26602304cdfd779b5a480e8202371
Laiba Adil <laibaadil30@gmail.com>
2025-08-15 19:58:12 +0500

    Updated POST route and tested API

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
-const express = require('express');
+/*const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('Hello World from Express!');
-});
-
-/*app.listen(3000, () => {
-  console.log('Server running on http://localhost:3000');
-});
-
-
-app.get('/about', (req, res) => {
-  res.send('This is the About page.');
 });*/
+
+const express = require('express');
+const app = express();
+
+app.use(express.json()); // Middleware to parse JSON
+
+const PORT = process.env.PORT || 5000;
+
+// GET Route - fetch data
+app.get('/', (req, res) => {
+    res.json({ message: 'Hello from GET route!' });
+});
+
+app.post('/api/message', (req, res) => {
+    res.json({ reply: 'POST request received' });
+});
+
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+
 
